abf97baf0bad10ecc679b3c913646368ecd1d0bd
Trần Tiến Hưng <57804828+constanreedjohn@users.noreply.github.com>
2023-02-10 11:06:07 +0700

    Update run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -1,7 +1,7 @@
 CUDA_VISIBLE_DEVICES=1 python test.py --Transformation TPS --FeatureExtraction ResNet --SequenceModeling BiLSTM --Prediction Attn \
-	--eval_data /data/hungtt/KLTN/SMILE/data_lmdb_release/evaluation \
+	--eval_data /data/data_lmdb_release/evaluation \
 	--benchmark_all_eval \
 	--batch_size 128 \
 	--workers 16 \
 	--data_filtering_off \
-	--saved_model /data/hungtt/FOMILE/best_accuracy.pth
+	--saved_model /weights/best_accuracy.pth
